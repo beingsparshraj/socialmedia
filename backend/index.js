3bd16a0431433ac3f415266d0533e42050d2f06c
sparsh <rajsparsh.2004@gmail.com>
2025-06-20 12:23:04 +0530

    see

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import connectDB from './utils/db.js';
 import userRoute from './routes/user.route.js';
 import postRoute from './routes/post.route.js';
 import messageRoute from './routes/message.route.js'
-import commentRoute from './routes/message.route.js';
 dotenv.config();
 import { app, server } from './socket/socket.js';
 const PORT = process.env.PORT;
@@ -29,7 +28,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/post', postRoute);
-app.use('/api/v1/comment', commentRoute);
 app.use('/api/v1/message', messageRoute);
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
